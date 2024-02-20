eeecff4522d4c94712b4e4e1972bf2ff2d395714
Hilmi AM <imiia75775@proton.me>
2024-02-21 05:48:50 +0700

    Initial commit

---
+++ src/main/java/franxx/code/cryptography/JavaCryptographyApplication.java
@@ -0,0 +1,13 @@
+package franxx.code.cryptography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCryptographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCryptographyApplication.class, args);
+	}
+
+}
