dccd74a1ff21592c10491887b84deeb0bdddddf5
Hilmi AM <imiia75775@proton.me>
2024-09-18 19:08:16 +0700

    create symmetric key pake algoritma AES

--- src/main/java/franxx/code/cryptography/SymmetricEncryptionUtil.java
+++ src/main/java/franxx/code/cryptography/SymmetricEncryptionUtil.java
@@ -6,8 +6,10 @@ import java.security.SecureRandom;
 
 public class SymmetricEncryptionUtil {
 
+    // Enkripsi yang pake kunci yang sama buat enkripsi dan dekripsi
     private static final String AES = "AES";
 
+    // membuat key udh di buat key nya... sekarang tinggal encrypt some data
     public static SecretKey createAESKey() throws Exception {
         SecureRandom secureRandom = new SecureRandom();
         KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
